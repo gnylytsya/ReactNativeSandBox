1e7851f738db0d8be397146b19a1a65d6952b834
gnylytsya <gnylytsya@gmail.com>
2018-04-17 21:51:17 +0300

    add Buttun + Linking

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View>
+      <View style={{flex: 1}}>
           <Header headerText={'Albums'} />
         <AlbumList/>
       </View>
--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
-import CardSection from './CardSecction'
+import CardSection from './CardSecction';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image, image} = album;
+    const {title, artist, thumbnail_image, image, url} = album;
     const {
         thumbnailStyle,
         headerContentStyle,
@@ -32,6 +33,9 @@ const AlbumDetail = ({album}) => {
                   style={imageStyle}
                   source={{ uri: image}}
               />
+          </CardSection>
+          <CardSection>
+              <Button onPress={() => Linking.openURL(url)}/>
           </CardSection>
       </Card>
     );
